e2c06ed27314958fd08b9e72abed5b28eb097eae
ngoquoc-git <qn11174@student.sac.edu>
2020-06-08 16:30:04 -0700

    Add 1-HelloWorld.py

---
+++ 1-Hello_World.py
@@ -0,0 +1,9 @@
+#This is the very first LESSON of every programmer
+
+#Command the computer to print out Hello World
+print("Hello WOrld")
+#Unlike other programming language, Python does not require semi-colon ";" at the end of every command 
+
+#The pound "#" sign at the beginning of every line indicates a comment command
+#The Ide will not read the comment line, these are notes for the programmers to read
+#THIS LINE IS A COMMENT AND WILL NOT BE READ
