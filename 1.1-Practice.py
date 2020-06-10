ed693408b3404d67e2a42cfcbf3231475654c5e6
ngoquoc-git <54878336+ngoquoc-git@users.noreply.github.com>
2020-06-10 12:44:09 -0700

    1.1 1.2 1.3 Practices
    Add practiced questions for sections 1.1 1.2 1.3 and solutions in each practice file

---
+++ 1.1-Practice.py
@@ -0,0 +1,12 @@
+#1.1 Hello World Practice
+
+#----------------------------------------
+
+#Question 1
+#Prompt the computer to print Hello World
+#Comment on your codes
+
+#----------------------------------------
+
+#Solution 1
+print ("Hello World")   #Hello World
