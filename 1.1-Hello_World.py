cc2d53a0443e279a8cde01bad2bb66c0970ef276
ngoquoc-git <54878336+ngoquoc-git@users.noreply.github.com>
2020-06-09 20:35:40 -0700

    Add 1.1-Hello_World and 1.2-Variables

---
+++ 1.1-Hello_World.py
@@ -0,0 +1,19 @@
+#1.1 Hello World!
+
+#This is always the very first LESSON of every programmer
+
+#----------------------------------------
+
+#Command the computer to print out Hello World
+
+print("Hello World!")
+
+#Unlike other programming language, Python does not require semi-colon ";" at the end of every command 
+
+#----------------------------------------
+
+#The pound "#" sign at the beginning of every line indicates a comment command
+
+#The Ide will not read the comment line, these are notes for the programmers to read
+
+#THIS LINE IS A COMMENT AND WILL NOT BE READ
---
+++ 1.2-Variables.py
@@ -0,0 +1,38 @@
+#1.2 Variables in Python
+
+#----------------------------------------
+
+#Variable is used to store data values
+
+#Unlike other language, Python does not need developer to declare the variable types
+
+#Variable type can be flexibly changed depends on user input
+
+a = 5       #declare variable a as an integer 
+print (a)
+
+a = "Tom"   #declare the variable a above as a string
+print (a)
+
+#Note: "Tom" is the same thing as 'Tom'
+
+#----------------------------------------
+
+#Naming Variable
+
+#Variable names can be lowercase, uppercase, number, and underscore
+
+firstvar = 0
+first_var = 0
+first_var_ = 0
+_first_var = 0
+FirstVar = 0
+FIRSTVAR = 0
+first1var = 0
+firstvar1 = 0
+
+#variable cannot contain hyphen, space and number at the beginning
+
+#first-var = 0
+#first var = 0
+#1firstvar = 0
