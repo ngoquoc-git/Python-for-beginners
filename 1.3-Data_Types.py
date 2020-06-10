cba29c60bda324a99b8eee70d24df465773d1392
ngoquoc-git <54878336+ngoquoc-git@users.noreply.github.com>
2020-06-09 21:32:46 -0700

    1.3-Data_Types.py
    Add a new section 1.3

---
+++ 1.3-Data_Types.py
@@ -0,0 +1,65 @@
+#1.3-Date Types in Python
+
+#----------------------------------------
+
+#Variables in Python can store different data type
+
+#Text
+a = "name"                  #str
+
+#Numeric
+b = 1                       #int
+c = 2.2                     #float
+d = 1j                      #complex
+
+#Sequence
+e = ["a", "b", "c"]         #list
+f = (1, 2, 3)               #tuple
+g = range(4)                #range
+
+#Mapping
+h = {"val": 1,"add": 22}    #dict
+
+#Set
+i = {"ab", "cd", "ef"}      #set
+g = frozenset({0, 1, 2})    #frozenset
+
+#Boolean
+k = True                    #boolean
+
+#Binary
+l = b"Hello"                #bytes
+m = bytearray(4)            #bytearray
+n = memoryview(bytes(3))    #memoryview
+
+#Print values
+print (a)
+print (b)
+print (c)
+print (d)
+print (e)
+print (f)
+print (g)
+print (h)
+print (i)
+print (g)
+print (k)
+print (l)
+print (m)
+print (n)
+
+#Print date type
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
+print(type(g))
+print(type(k))
+print(type(l))
+print(type(m))
+print(type(n))
