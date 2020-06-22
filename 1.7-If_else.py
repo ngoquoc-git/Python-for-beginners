5d6a7571b39a60a76201053412115acfc0f48009
ngoquoc-git <qn11174@student.sac.edu>
2020-06-22 11:01:56 -0700

    1.7 & 1.7  Practrice

--- 1.7-If_else.py
+++ 1.7-If_else.py
@@ -2,8 +2,42 @@
 
 #----------------------------------------
 
-#If is a logical condition with a following format
-# if (initial statment):
-#   (result statements)
+#IF is a logical condition with a following format
+#Note: Result statements MUST have at least one white space at the beginning of the line
+#      Programmers usually use 1 tab = 4 white spaces for each result statement
 
-#
+# if <Initial Statment>:
+#   <Result Statements>
+
+a = 3
+b = 2
+if a > b:
+    print ("a is greater than b, since 3 > 2")   
+
+#----------------------------------------
+
+#Some IF statements have more than one coditions
+#Python allows user to use ELIF for additional statements
+#An IF statement can have multiple ELIF but only have one if
+#IF has to be the first statement then ELIF
+
+#elif <Initial Statment>:
+#   <Result Statements>
+
+age = 25
+if age >=16 and age <= 18:
+    print("teenager")
+elif age >= 18:
+    print("adult")
+
+#----------------------------------------
+
+#Since an if statement may have infinite possible results
+#Python use ELSE to catches all conditions left
+#ELSE only need result statement 
+
+#else:
+#   <Result Statement>
+
+else:
+    print("child")
