344041abb8569bb9b8fa99858abf7c249739289d
Quoc Ngo <qn11174@student.sac.edu>
2020-06-29 22:25:43 -0700

    1.7 1.8 Practice

--- 1.8-Practice.py
+++ 1.8-Practice.py
@@ -14,6 +14,7 @@
 #----------------------------------------
 
 #Solution 1
+print("Question 1\n")
 answers = 'y'
 while answers == 'y':
     for i in range(1,11):
@@ -24,21 +25,51 @@ while answers == 'y':
 #Solution 2
 import random
 
-playerScore = 0
-computerScore = 0
-print("1 is ROCK, 2 is PAPER, or 3 is SCISSOR")
-while playerScore != 3 or computerScore!= 3:
-    print ("\nGame Score is now ", playerScore, " Player, ", computerScore) #Scores Tracker  
-    
-    playerChoice = input("PLease choose an option: ")       #Get inputs from the user
-    computerChoice = random.randrange(1,3)                  #Generate a random decision for the computer
-    
-    if playerChoice == 1:                                   #Convert player's decisions      
-        playerDecision = "ROCK"
-    elif playerChoice == 2:
-        playerDecision = "PAPER"
-    elif playerChoice == 3:
-        playerDecision = "SCISSOR"
-    else:
-        playerChoice = "INVALID ENTRY"
+loop = 'y'
+playerScore = computerScore = 0
+playerChoice = computerChoice =0
+playerDecision = computerDecision = 'INVALID ENTRY'
+print("\nQuestion 2\n")
 
+while loop == 'y':                                              #play multiple games
+    playerScore = computerScore = 0
+    print ("\nGame Score is now ", playerScore, " Player, ", computerScore, " Computer") #Scores Tracker
+    while playerScore < 3 and computerScore < 3:  
+        print("\n1 is ROCK, 2 is PAPER, or 3 is SCISSOR")
+
+        playerChoice = int(input("PLease choose an option: "))  #Get inputs from the user
+        computerChoice = random.randrange(1,4)                  #Generate a random decision for the computer
+        
+        while playerDecision == "INVALID ENTRY":
+            if playerChoice == 1:                                   #Convert player's decisions      
+                playerDecision = "ROCK"
+            elif playerChoice == 2:
+                playerDecision = "PAPER"
+            elif playerChoice == 3:
+                playerDecision = "SCISSOR"
+            else:
+                playerDecision = "INVALID ENTRY"
+
+        if computerChoice == 1:                                   #Convert computer's decisions      
+            computerDecision = "ROCK"
+        elif computerChoice == 2:
+            computerDecision = "PAPER"
+        else:
+            computerDecision = "SCISSOR"
+
+        print ("\nPlayer chose ", playerDecision)
+        print ("Computer chose ", computerDecision)
+
+        if (computerChoice % 3) + 1 == playerChoice:              #Win - Lose conditions
+            print("Player wins")
+            playerScore += 1
+        elif (playerChoice % 3) + 1 == computerChoice:
+            print("Computer wins")
+            computerScore += 1
+        else: print ("Draw")
+        print ("\nGame Score is now ", playerScore, " Player, ", computerScore, " Computer") #Scores Tracker
+    if playerScore == 3:
+        print("Player wins with 3 scores")
+    else:
+        print("Computer wins with 3 scores")
+    loop = input("\nDo you want to play another game: ").lower()
